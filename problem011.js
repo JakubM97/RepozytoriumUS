d4daeccb0cefefed27b8023993447735579615da
JakubM97 <44525522+JakubM97@users.noreply.github.com>
2018-11-01 22:39:10 +0100

    Update problem011.js

--- problem011.js
+++ problem011.js
@@ -1,8 +1,8 @@
-function deepFind(a,b) {
+function deepFindAndCount(a,b) {
   var suma=0;
   for (var i=0; i<=a.length; i++) {
     if(typeof a[i]=='object') {
-       suma+=deepFind(a[i],b);
+       suma+=deepFindAndCount(a[i],b);
        } else {
          if (a[i]==b) suma++;
        }
