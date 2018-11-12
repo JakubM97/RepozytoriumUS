cc57c5a685a2b77877ab29fccbd3bb44c48703fb
JakubM97 <44525522+JakubM97@users.noreply.github.com>
2018-11-12 16:25:42 +0100

    Update problem014.js

--- problem014.js
+++ problem014.js
@@ -1,24 +1,22 @@
 const Vectors = {
   sum : function(a,b) {
-    if(typeof a=='object' || typeof a=='number' && typeof b=='object' || typeof b=='number') {
-    for(var i=0; i<b.length; i++) {
-      if(typeof a[i]!='number') a[i]=0;
-      a[i]+=b[i];
-    }
-    return a; }
-    else { return false; }
-  
+    if(a.length==b.length) {
+      var wynik = [];
+      for(var i=0; i<b.length; i++) {
+        wynik[i]=a[i]+b[i];
+      }
+      return wynik;
+    } else return false;
   },
   
   mulByScalar : function(a,b) {
-    if ((typeof a=='object' || typeof a=='number') && typeof b=='number') {
-      if(typeof a=='number') return a*b
-      else {
-        for(var i=0; i<a.length; i++){
-          a[i]=a[i]*b;
-        }}
-      return a;
-    } else { return false; }
+    if (typeof a=='object' && typeof b=='number') {
+      var wynik = [];  
+      for(var i=0; i<a.length; i++){
+          wynik[i]=a[i]*b;
+        }
+      return wynik;
+    } else return false;
     
   }
 }
